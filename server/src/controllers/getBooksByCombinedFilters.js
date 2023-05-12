52b8453c34f7270b734ff5de11702a97a545c002
Nataly Revelo <natazarama@hotmail.com>
2023-05-12 17:42:13 -0400

    Combined filters implementation by query params

---
+++ server/src/controllers/getBooksByCombinedFilters.js
@@ -0,0 +1,41 @@
+const { QueryTypes } = require('sequelize');  /// uso RAW QUERIES - replacement from sequelize
+const { conn } = require('../db.js')
+
+
+const getBookByCombinedFilters =  async ( title, genre, author) => {
+    let query = `SELECT * FROM "Books" WHERE`;
+    let needsAnd = false
+    if(title) {
+        query += ` title ILIKE '%${title}%' `
+        needsAnd = true;
+    }
+    if(genre) {
+        if(needsAnd) {
+            query += ' AND '
+        }
+        query += ` genre = '${genre}' `;
+        needsAnd = true;
+    }
+    if(author) {
+        if(needsAnd) {
+            query += ' AND '
+        }
+        query += ` author = '${author}' `;
+        needsAnd = true;
+    }
+    const booksByCombinedFilters = await conn.query(
+        query,
+        {
+            type: QueryTypes.SELECT
+        }
+    );
+
+    if(booksByCombinedFilters.length > 0) {
+        return booksByCombinedFilters;
+    }
+    else {
+        throw new Error("There is not books with that title")
+    }
+}
+
+module.exports = getBookByCombinedFilters;
