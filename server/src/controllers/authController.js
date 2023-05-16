074ecbfb84ebc365b7150eb2ef2865278cd8e91e
Nataly Revelo <natazarama@hotmail.com>
2023-05-16 08:59:48 -0500

    Login adjustments, first steps token .

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,8 @@
 const { User } = require("../db");
 const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const { JWT_PRIVATE_KEY, JWT_PUBLIC_KEY } = process.env;
+
 
 const authController = async (email, password) => {
   try {
@@ -18,6 +21,10 @@ const authController = async (email, password) => {
     }
     const cleanUser = user.dataValues
     delete cleanUser.password
+
+    // ///implementacion JWT
+    // cleanUser.token= generateJWT(cleanUser)
+
     return cleanUser;
 
   } catch (error) {
@@ -25,5 +32,9 @@ const authController = async (email, password) => {
     throw error;
   }
 };
+
+// function generateJWT(cleanUser) {
+//  return jwt.sign(cleanUser, JWT_PRIVATE_KEY,{ expiresIn: 3600, algorithm: 'RS256'});
+// }
 
 module.exports = authController;
