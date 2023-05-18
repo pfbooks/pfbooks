4b187a179473792bcca604a40b3f53d86d77ee18
MarcosNovella <marcosnovella99@gmail.com>
2023-05-18 14:38:19 -0300

    agregando funcionalidad al componente profile

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,10 +1,30 @@
+import {useDispatch} from 'react-redux'
+import { useEffect } from "react"
+import { userById } from "../../redux/actions/actions"
+
 const Profile = () => {
-    return(
+
+
+    const user = JSON.parse(localStorage.getItem('user'))
+    const userId = user ? user.id : ''
+    const dispatch = useDispatch()
+
+    useEffect( () =>{
+        dispatch(userById(userId))
+    }, [dispatch, userId])
+
+    return (
         <div>
-            <img src={'holis'} alt='image not found'/>
-            <p>Name:</p>
-            <p>Lastname:</p>
-            <p>Email:</p>
+            {user ? (
+                <>
+                    <img src={'holis'} alt='image not found'/>
+                    <p>Name: {user.name}</p>
+                    <p>Lastname: {user.lastName}</p>
+                    <p>Email: {user.email}</p>
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
     )
 }
