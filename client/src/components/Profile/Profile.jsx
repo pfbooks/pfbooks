df0b8b66eaaa8ea904408998097a4d2987297b8a
MarcosNovella <marcosnovella99@gmail.com>
2023-05-18 13:40:35 -0300

    creating Profile component and adding a view of it into App.js

---
+++ client/src/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+const Profile = () => {
+    return(
+        <div>
+            <img src={'holis'} alt='image not found'/>
+            <p>Name:</p>
+            <p>Lastname:</p>
+            <p>Email:</p>
+        </div>
+    )
+}
+
+export default Profile
