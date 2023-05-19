e07418609166f086d98ef8f96b6e99db5c9d66dc
Nataly Revelo <natazarama@hotmail.com>
2023-05-18 22:19:31 -0500

    Email notifications implementation

--- server/src/controllers/authGoogleController.js
+++ server/src/controllers/authGoogleController.js
@@ -4,6 +4,13 @@ const jwt = require("jsonwebtoken");
 const sendEmail = require("../emailNotifications/emailNotification");
 const { GOOGLE_CLIENT_ID, JWT_KEY } = process.env;
 const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+const USERNAME_PLACEHOLDER = "${userName}"
+const REGISTRATION_MESSAGE = "Hola " + USERNAME_PLACEHOLDER + ".\n\n" +
+    "Te informamos que tu registro en nuestro sitio web fue exitoso.\n" +
+    "A partir de ahora, podrás acceder a nuestros servicios y funcionalidades.\n" +
+    "Gracias por unirte a SERENDIPIA, esperamos disfrutes de la experiencia.\n\n" +
+    "Saludos cordiales.\n" +
+    "SERENDIPIA"
 
 
 const authGoogleController = async (credential) =>  {
@@ -28,7 +35,10 @@ const authGoogleController = async (credential) =>  {
                 email:ticket.getPayload().email,
                 password:getRandomPassword(),
             })
-            sendEmail(createdUser.email);
+            sendEmail(
+                createdUser.email,
+                `Registro exitoso del usuario : ${createdUser.email}`,
+                REGISTRATION_MESSAGE.replace(USERNAME_PLACEHOLDER, createdUser.name + " " + createdUser.lastName));
             return generateUserResponse(createdUser)
         }
 
--- server/src/emailNotifications/emailNotification.js
+++ server/src/emailNotifications/emailNotification.js
@@ -1,36 +1,27 @@
 const {google} = require('googleapis');
-const { GOOGLE_CLIENT_ID, CLIENT_SECRET, YOUR_REDIRECT_URL  } = process.env
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GMAIL_API_REFRESH_TOKEN} = process.env
 
 
 const OAuth2 = google.auth.OAuth2;
 
 const oauth2Client = new OAuth2(
     GOOGLE_CLIENT_ID,
-    CLIENT_SECRET,
-    YOUR_REDIRECT_URL
+    GOOGLE_CLIENT_SECRET
 );
+oauth2Client.setCredentials({
+    refresh_token : GMAIL_API_REFRESH_TOKEN
+})
 
+const  sendEmail = (destinationEmail, emailSubject, emailBody) => {
 
-const  sendEmail = (destinationEmail) => {
     const gmail = google.gmail({version: 'v1', auth: oauth2Client});
 
     const message = [
         `To: ${destinationEmail}`,
         "From: pfbooks25@gmail.com",
-        "Subject: Registro Exitoso",
+        `Subject: ${emailSubject}`,
         "",
-        "Hola Usuario,",
-        "",
-        "te informamos que tu registro en nuestro sitio web fue exitoso.",
-        "",
-        "A partir de ahora, podrás acceder a nuestros servicios y funcionalidades",
-        "",
-        " Gracias por unirte a SERENDIPIA, esperamos disfrutes de la experiencia",
-        "",
-        "Saludos cordiales,",
-        "",
-        "SERENDIPIA"
-    ].join('\n');
+        emailBody].join('\n');
 
     const encodedMessage = Buffer.from(message)
         .toString('base64')
@@ -45,10 +36,10 @@ const  sendEmail = (destinationEmail) => {
         }
     },(err,res) => {
         if(err){
-            console.log('La Api retorna un error' + err );
+            console.log('There was an error sending email notification : ' + err );
             return;
         }
-        console.log('Message sent:', res.data);
+        console.log('Email notification was successfully sent : ', res.data);
 
     })
 
