8254fdc143bc7bc1ce4e4fe03866135d596c3e39
Nataly Revelo <natazarama@hotmail.com>
2023-05-25 14:02:48 -0500

    Correction 401 y 404 error. Fixes en action get all users,  get user by Ig y App front.

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,6 @@ import { ToastProvider } from 'react-toast-notifications';
 import 'react-toastify/dist/ReactToastify.css';
 import Register from "./components/Form/Register";
 import LoginForm from "./components/Login/LoginForm";
-import axios from "axios";
-import { getUserData } from "./redux/actions/actions";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
 import Chart from "./components/Chart/Chart";
@@ -18,22 +14,6 @@ import BooksTable from "./components/BooksTable/BooksTable";
 import UsersTable from "./components/UsersTable/UsersTable";
 
 function App() {
-  const ENDPOINT_USER = "http://localhost:3001/user";
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-      axios
-        .get(`${ENDPOINT_USER}`, { headers: { Authorization: `Bearer ${user.token}` } }, { withCredentials: true })
-        .then((response) => {
-          dispatch(getUserData(response.data.user));
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }, [dispatch]);
 
   return (
     <ToastProvider>
