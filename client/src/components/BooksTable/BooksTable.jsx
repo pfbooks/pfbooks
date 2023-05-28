d3bfe8faf508851a1b8bb0ff7a392ccfc6e32b43
MarcosNovella <marcosnovella99@gmail.com>
2023-05-28 20:14:20 -0300

    some changes

--- client/src/components/BooksTable/BooksTable.jsx
+++ client/src/components/BooksTable/BooksTable.jsx
@@ -4,36 +4,47 @@ import { allBooks } from "../../redux/actions/actions";
 import { DataGrid } from '@mui/x-data-grid';
 
 
-const columns = [
-    {
-        field: "image",
-        headerName: "Image",
-        renderCell: (params) => (
-            <div style={{ width: 50, height: 50, borderRadius: '50%', overflow: 'hidden' }}>
-                <img src={params.value} alt="Book Cover" style={{ width: '100%', height: '100%' }} />
-            </div>
-        ),
-        width: 100,
-    },
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'title', headerName: 'Title', width: 300 },
-    { field: 'author', headerName: 'Author', editable: true , width: 240 },
-    {
-        field: 'price',
-        headerName: 'Price',
-        type: 'number',
-        width: 90,
-        renderCell: (params) => (
-          <div>
-            $ {params.value}
-          </div>
-        ),
-      },
-    { field: 'rating', headerName: 'Rating', type: 'number', width: 160 },
-    { field: 'genre', headerName: 'Genre', width: 200}
-  ];
 
 const BooksTable = (props) => {
+    const columns = [
+        {
+            field: "image",
+            headerName: "Image", editable: true, 
+            renderCell: (params) => (
+                <div style={{ width: 50, height: 50, borderRadius: '50%', overflow: 'hidden' }}>
+                    <img src={params.value} alt="Book Cover" style={{ width: '100%', height: '100%' }} />
+                </div>
+            ),
+            width: 100,
+        },
+        { field: 'id', headerName: 'ID', editable: true , width: 70 },
+        { field: 'title', headerName: 'Title', editable: true , width: 300 },
+        { field: 'author', headerName: 'Author', editable: true , width: 240 },
+        {
+            field: 'price',
+            headerName: 'Price',
+            type: 'number',
+            width: 90,
+            editable: true,
+            renderCell: (params) => (
+              <div>
+                $ {params.value}
+              </div>
+            ),
+          },
+        { field: 'rating', headerName: 'Rating', type: 'number', editable: true , width: 160 },
+        { field: 'genre', headerName: 'Genre', editable: true , width: 200},
+        {
+            field: 'action',
+            headerName: 'Action',
+            width: 100,
+            renderCell: (params) => (
+                <button onClick={() => console.log(params.row)}>
+                    Save
+                </button>
+            ),
+        },
+      ];
     const books = useSelector((state) => state.books);
     const dispatch = useDispatch()
     useEffect(()=>{
