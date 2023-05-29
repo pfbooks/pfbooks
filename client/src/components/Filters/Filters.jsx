467113eb171d43b44a1296a7c83eae8a9bfa97d3
Jorge Jimenez <jorge970102@outlook.com>
2023-05-28 19:29:54 -0500

    fix/ select de filtros (tiene bugs por solucionar)

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -5,11 +5,20 @@ import styles from './Filters.module.css';
 
 const Filters = ({ handlePageChange }) => {
   const dispatch = useDispatch();
+  const books = useSelector((state) => state.books);
   const genres = useSelector((state) => state.genres);
   const authors = useSelector((state) => state.authors);
   const [order, setOrder] = useState('');
   const [genre, setGenre] = useState('');
   const [author, setAuthor] = useState('');
+  const [gen, setGen] = useState([]);
+  const [aut, setAut] = useState([]);
+
+  useEffect(() => {
+    setGen(genres)
+    setAut(authors)
+
+  }, [genres, authors]);
 
   useEffect(() => {
     dispatch(allBooks());
@@ -24,9 +33,22 @@ const Filters = ({ handlePageChange }) => {
   const handleFilter = (event) => {
     if (event.target.name === 'genre' && event.target.value !== 'All') {
       setGenre(event.target.value);
+      let arr = books?.filter(book => book.genre.includes(event.target.value))
+      let newAut = arr.map(book => book.author)
+      const uniqueAut = newAut.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+      });
+      setAut(uniqueAut);
     }
     if (event.target.name === 'author' && event.target.value !== 'All') {
       setAuthor(event.target.value);
+      let arr = books?.filter(book => book.author.includes(event.target.value))
+      let newGen = arr.map(book => book.genre)
+      let flatGen = newGen.flat()
+      const uniqueGens = flatGen.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+      });      
+      setGen(uniqueGens);
     }
     setOrder(event.target.value + order);
     handlePageChange(1);
@@ -34,6 +56,8 @@ const Filters = ({ handlePageChange }) => {
 
   const handleAllBooks = () => {
     dispatch(allBooks());
+    setAut(authors)
+    setGen(genres)
     setGenre('');
     setAuthor('');
   };
@@ -42,14 +66,14 @@ const Filters = ({ handlePageChange }) => {
     <div>
       <select className={styles.selectFilters} onChange={handleFilter} name='genre' value={genre}>
         <option value="">By genre</option>
-        {genres.map(genre => (
+        {gen?.map(genre => (
           <option key={genre} value={genre}>{genre}</option>
         ))}
       </select>
 
       <select className={styles.selectFilters} onChange={handleFilter} name='author' value={author}>
         <option value="">By author</option>
-        {authors.map(author => (
+        {aut?.map(author => (
           <option key={author} value={author}>{author}</option>
         ))}
       </select>
