ce0e27a57809565bc6d6e4da0fcb62eac7932a26
MarcosNovella <marcosnovella99@gmail.com>
2023-05-30 00:59:05 -0300

    updating package.json

--- client/src/components/Form/Register.jsx
+++ client/src/components/Form/Register.jsx
@@ -4,9 +4,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import styles from "./Register.module.css";
 import * as yup from "yup";
-import { useToasts } from "react-toast-notifications";
 import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { FaHome } from "react-icons/fa";
 
 const Register = () => {
   const schema = yup.object().shape({
@@ -36,13 +34,11 @@ const Register = () => {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const { addToast } = useToasts();
   const history = useHistory();
 
   const onSubmit = (data) => {
     dispatch(createUser(data))
       .then(() => {
-        addToast("Usuario creado correctamente", { appearance: "success" });
         history.push("/login")
       })
       .catch((error) => {
