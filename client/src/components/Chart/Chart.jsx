98e77c3e3081af0a286024997bd962abbab284f2
MarcosNovella <marcosnovella99@gmail.com>
2023-05-30 01:58:03 -0300

    adding some photos to about

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -7,7 +7,7 @@ import {
   faTrash,
   faDollarSign,
 } from "@fortawesome/free-solid-svg-icons";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, Link } from "react-router-dom/cjs/react-router-dom.min";
 import styles from "./Chart.module.css";
 import ButtonMP from "../Payment/ButtonMP";
 import { CartContext } from "../../context/cart";
