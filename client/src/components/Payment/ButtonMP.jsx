045e1439f0eea5bb4104c124559c17135e401034
Jorge Jimenez <jorge970102@outlook.com>
2024-04-17 19:43:27 -0500

    fix endpoints for deploy

--- client/src/components/Payment/ButtonMP.jsx
+++ client/src/components/Payment/ButtonMP.jsx
@@ -14,7 +14,7 @@ initMercadoPago("TEST-75896b1c-2143-43c1-82c4-274c670747f1");
 // Then some document.querySelector('.cho-container') to get the element
 // and then use the mp.checkout() method
 
-const endpoint = "http://localhost:3001";
+const endpoint = "https://pfbooks.onrender.com";
 // const obj = {
 //     user: {
 //         name: 'Jorge',
