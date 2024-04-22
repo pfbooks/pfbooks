27b5eb53a276bf9e1cf7770a2b8eb9edf8935532
Jorge Jimenez <jorge970102@outlook.com>
2024-04-22 12:08:39 -0500

    Add tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+}
+
+
